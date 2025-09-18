ce7760826c835750c214aaa5bd516524f3184f9d
MateuszSzczukiewicz <szcmat6277@gmail.com>
2025-09-18 12:29:48 +0200

    added hamming

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,3 +1,17 @@
 #include "hamming.h"
 
-int compute(const char *lhs, const char *rhs);
+int compute(const char *lhs, const char *rhs) {
+  int counter = 0;
+  int lhs_len = 0;
+  int rhs_len = 0;
+
+  while ()
+
+    for (int i = 0; lhs[i] != '\0' && rhs[i] != '\0'; i++) {
+      if (lhs[i] != rhs[i]) {
+        counter++;
+      }
+    }
+
+  return counter;
+}

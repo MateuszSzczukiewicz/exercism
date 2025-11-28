426c815c25859878a72a61ff653a11b97a32cb2f
MateuszSzczukiewicz <szcmat6277@gmail.com>
2025-11-28 10:08:14 +0100

    edit c/high_score

--- c/high-scores/high_scores.c
+++ c/high-scores/high_scores.c
@@ -1,15 +1,19 @@
 #include "high_scores.h"
 #include <stdint.h>
 
-int32_t latest(const int32_t *scores, size_t scores_len) {
-  return scores[scores_len + 1];
+int32_t latest(const int32_t *scores, size_t scores_len)
+{
+  return scores[scores_len - 1];
 }
 
-int32_t personal_best(const int32_t *scores, size_t scores_len) {
+int32_t personal_best(const int32_t *scores, size_t scores_len)
+{
   int32_t biggest_score = *scores;
 
-  for (int i = 0; i < scores_len; i++) {
-    if (scores[i] > biggest_score) {
+  for (int i = 0; i < (int)scores_len; i++)
+  {
+    if (scores[i] > biggest_score)
+    {
       biggest_score = scores[i];
     }
   }
@@ -17,21 +21,26 @@ int32_t personal_best(const int32_t *scores, size_t scores_len) {
   return biggest_score;
 }
 
-/// Write the highest scores to `output` (in non-ascending order).
-/// Return the number of scores written.
 size_t personal_top_three(const int32_t *scores, size_t scores_len,
-                          int32_t *output) {
+                          int32_t *output)
+{
   int32_t biggest_scores[3] = {0};
 
-  for (int i = 0; i < 3; i++) {
-    biggest_scores[i] = scores[i];
-  }
-
-  for (int i = 0; i < scores_len; i++) {
-    if (scores[i] > biggest_scores) {
-      biggest_scores = scores[i];
+  for (int i = 0; i < (int)scores_len; i++)
+  {
+    if (i == 0)
+    {
+      biggest_scores[0] = scores[0];
+    } else if (biggest_scores[i - 1] && scores[i] > biggest_scores[i - 1])
+    {
+      do {
+        biggest_scores[i] = biggest_scores[i - 1];
+        biggest_scores[i - 1] = scores[i];
+      } while (i >= 2 && scores[i] > biggest_scores[i - 2]);
     }
   }
 
-  return *biggest_scores;
+  *output = *biggest_scores;
+
+  return scores_len;
 }

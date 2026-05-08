fe22363dc764e6787708d43e7a93478a04dc793e
MateuszSzczukiewicz <szcmat6277@gmail.com>
2026-05-08 11:21:36 +0200

    feat: finish few exercises

--- c/pangram/pangram.c
+++ c/pangram/pangram.c
@@ -1,3 +1,29 @@
 #include "pangram.h"
+#include <ctype.h>
+#include <stddef.h>
 
-bool is_pangram(const char *sentence) {}
+#define NUMBER_OF_LETTERS 26
+
+bool is_pangram(const char *sentence) {
+  if (sentence == NULL) {
+    return false;
+  }
+
+  int seen[NUMBER_OF_LETTERS] = {0};
+
+  for (size_t i = 0; sentence[i] != '\0'; ++i) {
+    if (isalpha((unsigned char)sentence[i])) {
+      int letter_index = tolower((unsigned char)sentence[i]) - 'a';
+
+      seen[letter_index] = 1;
+    }
+  }
+
+  for (size_t i = 0; i < NUMBER_OF_LETTERS; ++i) {
+    if (seen[i] == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
